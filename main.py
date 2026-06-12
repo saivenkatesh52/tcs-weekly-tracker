9ea8296b542c373f85ca61dcc7c423e5cf12dd70
saivenkatesh52 <saivenkatesh52@gmail.com>
2026-06-12 14:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import requests
 
+print("TCS Tracker Started")
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": "✅ TCS Tracker Test Message from Railway"
+    }
+)
+
+print(response.text)
